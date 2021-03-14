9dc81d4bc79195abb970cebe5cd02b928fd75878
Thuan <ducthuan232tn@gmail.com>
2021-03-14 09:55:59 +0700

    1

--- src/Quydoingay.java
+++ src/Quydoingay.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Quydoingay {
     public static void ngaytrongtuan(int m, int d, int y) {
         int y0 = y - (14 - m) / 12;System.out.println(y0);
-        int x = y0 * (1 + (1 / 4) - (1 / 100) + (1 / 400));System.out.println(x);
+        int x = y0 +y0/4-y0/100+y0/400;System.out.println(x);
         int        m0 = m + 12 * ((14 - m) / 12) - 2;System.out.println(m0);
         int d0 = (d + x + 31 * m0 / 12) % 7;System.out.println(d0);
         String ngay = "";
